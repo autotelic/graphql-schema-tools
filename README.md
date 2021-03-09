f8fa4bc682c59ac8f30f908ee39e9016efeede71
Holden Whitehead <28017008+HW13@users.noreply.github.com>
2021-03-09 14:46:43 -0800

    feat: Add normalizeGQLSource

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
 # gql-schema-normalizer
+
 Normalize GraphQL string schemas.
+
+### Usage
+
+```sh
+npm i @autotelic/gql-schema-normalizer
+```
+
+When passed a string GQL source, `normalizeGQLSource` groups top-level declarations by kind and then alphabetizes them along with their fields, directives, arguments, values, types, interfaces, and locations - all by name.
+
+#### Example
+
+```js
+const normalizeGQLSource = require('@autotelic/gql-schema-normalizer')
+
+const schema = `
+  type Query {
+    products(priceUnder: Float priceOver: Float category: String): [Products]! @paginate @cacheControl(maxAge: 2000 scope: PUBLIC)
+    customer(id: ID): Customer
+  }
+
+ type Product {
+    name: String!
+    price: Float!
+    category: String!
+    id: ID!
+  }
+
+  type Customer @cacheControl(scope: PRIVATE maxAge: 1000) {
+    name: String!
+    address: String!
+    id: ID!
+  }
+
+  directive @paginate on FIELD_DEFINITION
+  directive @cacheControl(scope: CacheScope maxAge: Int) on OBJECT | FIELD_DEFINITION
+
+  enum CacheScope {
+    PUBLIC
+    PRIVATE
+  }
+`
+
+const { source, error } = normalizeGQLSource(schema)
+```
+
+Given the above `schema`, the returned `source` would look like:
+
+```gql
+directive @cacheControl(maxAge: Int, scope: CacheScope) on FIELD_DEFINITION | OBJECT
+
+directive @paginate on FIELD_DEFINITION
+
+enum CacheScope {
+  PRIVATE
+  PUBLIC
+}
+
+type Customer @cacheControl(maxAge: 1000, scope: PRIVATE) {
+  address: String!
+  id: ID!
+  name: String!
+}
+
+type Product {
+  category: String!
+  id: ID!
+  name: String!
+  price: Float!
+}
+
+type Query {
+  customer(id: ID): Customer
+  products(category: String, priceOver: Float, priceUnder: Float): [Products]! @cacheControl(maxAge: 2000, scope: PUBLIC) @paginate
+}
+```
+
+### API
+
+#### `normalizeGQLSource`: `(source: string) => object`
+
+Returns an object with the following properties:
+  - `source`: `string`- The normalized GQL source.
+  - `error`: `GraphQLError | undefined` - If an error occurs, it will be added here.
---
+++ index.test.js
@@ -0,0 +1,224 @@
+const { test } = require('tap')
+const normalizeGQLSource = require('.')
+
+// Note: the inconsistent spacing and indentation is intentional and part of the test.
+const sourceOne = `
+  directive @paginate on FIELD_DEFINITION
+type Human implements Mammal & FriendlyMammal & Friendly @auth(requires: USER code: 99) {
+  id: ID!
+  name: String!
+  email: String!
+  friends: [Friend]
+  address: Address
+}
+
+extend type Query {
+  friendsOf(zebraID: ID humanID: ID): [Friend] @paginate @awesome
+  friend(zebraID: ID humanID: ID): Friend
+}
+
+interface Mammal  {
+  id: ID!
+name: String!
+}
+
+interface Friendly {
+  friends: [Friend]
+}
+
+interface FriendlyMammal implements Mammal & Friendly {
+  friends: [Friend]
+  name: String!
+  id: ID!
+}
+
+interface Striped {
+    stripes: Int!
+}
+
+type Address {
+  street: String!
+  buildingNum: String!
+  zipCode: String!
+  city: String!
+  country: String!
+}
+type Zebra implements Striped & Friendly & FriendlyMammal & Mammal @awesome @auth(code: 52, requires: UNKNOWN) {
+  name: String!
+  id: ID!
+  friends: [Friend]
+  stripes: Int!
+}
+
+directive @awesome on OBJECT | FIELD_DEFINITION
+union Friend = Zebra | Human
+
+directive @auth(
+  code: Int
+requires: Role = ADMIN
+) on OBJECT | FIELD_DEFINITION
+
+      enum Role { REVIEWER ADMIN USER UNKNOWN}`
+
+// Same overall schema as sourceOne, just with different spacing and ordering.
+const sourceTwo = `
+type Address {
+  buildingNum: String!
+  city: String!
+  zipCode: String!
+  country: String!
+  street: String!
+}
+  interface Striped {
+ stripes: Int!
+}
+
+ extend type Query {
+  friendsOf(zebraID: ID humanID: ID): [Friend] @paginate @awesome
+  friend(zebraID: ID humanID: ID): Friend
+}
+
+
+directive @paginate on FIELD_DEFINITION
+interface FriendlyMammal implements Friendly & Mammal  {
+  friends: [Friend]
+  name: String!
+  id: ID!
+}
+
+type Zebra implements FriendlyMammal & Friendly & Striped & Mammal @awesome @auth(requires: UNKNOWN code: 52) {
+  stripes: Int!
+  friends: [Friend]
+  name: String!
+  id: ID!
+}
+
+enum Role {
+  UNKNOWN
+  ADMIN
+  REVIEWER
+  USER
+}
+
+type Human implements Friendly & FriendlyMammal & Mammal @auth(requires: USER code: 99) {
+  email: String!
+  name: String!
+  address: Address
+  friends: [Friend]
+  id: ID!
+}
+
+interface Friendly {
+  friends: [Friend]
+}
+directive @awesome on OBJECT | FIELD_DEFINITION
+interface Mammal  {
+  name: String!
+  id: ID!
+}
+union Friend = Zebra | Human
+
+directive @auth(
+  requires: Role = ADMIN
+  code: Int
+) on  OBJECT | FIELD_DEFINITION
+
+`
+
+const expected = `directive @auth(code: Int, requires: Role = ADMIN) on FIELD_DEFINITION | OBJECT
+
+directive @awesome on FIELD_DEFINITION | OBJECT
+
+directive @paginate on FIELD_DEFINITION
+
+enum Role {
+  ADMIN
+  REVIEWER
+  UNKNOWN
+  USER
+}
+
+interface Friendly {
+  friends: [Friend]
+}
+
+interface FriendlyMammal implements Friendly & Mammal {
+  friends: [Friend]
+  id: ID!
+  name: String!
+}
+
+interface Mammal {
+  id: ID!
+  name: String!
+}
+
+interface Striped {
+  stripes: Int!
+}
+
+type Address {
+  buildingNum: String!
+  city: String!
+  country: String!
+  street: String!
+  zipCode: String!
+}
+
+type Human implements Friendly & FriendlyMammal & Mammal @auth(code: 99, requires: USER) {
+  address: Address
+  email: String!
+  friends: [Friend]
+  id: ID!
+  name: String!
+}
+
+type Zebra implements Friendly & FriendlyMammal & Mammal & Striped @auth(code: 52, requires: UNKNOWN) @awesome {
+  friends: [Friend]
+  id: ID!
+  name: String!
+  stripes: Int!
+}
+
+union Friend = Human | Zebra
+
+extend type Query {
+  friend(humanID: ID, zebraID: ID): Friend
+  friendsOf(humanID: ID, zebraID: ID): [Friend] @awesome @paginate
+}
+`
+
+test(
+  'should group top-level declarations by kind and then alphabetize them along with their fields, directives, arguments, values, types, interfaces, and locations by name.',
+  async ({ is }) => {
+    const { source: sourceOneResult } = normalizeGQLSource(sourceOne)
+    const { source: sourceTwoResult } = normalizeGQLSource(sourceTwo)
+    is(sourceOneResult, sourceTwoResult, 'once normalized, schemas should be exactly the same if there were only stylistic differences previously.')
+    is(sourceOneResult, expected)
+    is(sourceTwoResult, expected)
+  })
+
+test(
+  'should provide a snippet of where a parse error occurs.',
+  async ({ is }) => {
+    const schemaWithErrorOne = `
+      type Query foo: String
+      `
+
+    const schemaWithErrorTwo = `
+      type Query {
+        foo(): String
+      }`
+
+    const { error: errorOne } = normalizeGQLSource(schemaWithErrorOne)
+    const { error: errorTwo } = normalizeGQLSource(schemaWithErrorTwo)
+
+    is(errorOne.message, 'Syntax Error: Unexpected Name "foo". Found near: `Query foo:`.')
+    is(errorTwo.message, 'Syntax Error: Expected Name, found ")". Found near: `{ foo():`.')
+  })
+
+test(
+  'should throw an Error if not passed a string',
+  async ({ throws }) => {
+    throws(() => normalizeGQLSource(), Error('gql-schema-normalizer: Expected a string.'))
+  })
